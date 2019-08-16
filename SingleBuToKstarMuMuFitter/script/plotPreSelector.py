a17163f9d245dd8ca64686b04cd8ba5c9b355689
Po-Hsun Chen <pchen@cern.ch>
2019-08-16 17:11:10 +0200

    Better fitting

---
+++ SingleBuToKstarMuMuFitter/script/plotPreSelector.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# vim: set sts=4 sw=4 fdm=indent fdl=1 fdn=3 ft=python et:
+
+import os
+
+import ROOT
+import SingleBuToKstarMuMuFitter.plotCollection as plotCollection
+
+def create_histo():
+    tree = ROOT.TChain("tree")
+    tree.Add("/eos/cms/store/user/pchen/BToKstarMuMu/dat/ntp/v3p2/BuToKstarMuMu-data-2012*.root")
+
+    fout = ROOT.TFile("h_plotPreSelector.root", "RECREATE")
+
+    tree.Draw("sqrt(2*sqrt(pippx*pippx+pippy*pippy+pippz*pippz+0.13957*0.13957)*sqrt(pimpx*pimpx+pimpy*pimpy+pimpz*pimpz+0.13957*0.13957)-2*(pippx*pimpx+pippy*pimpy+pippz*pimpz)+2*0.13956*0.13957)>>h_kshortMass_presel(200,0.4,0.6)")
+
+    fout.Write()
+    fout.Close()
+
+def plot_histo(fname="h_plotPreSelector.root"):
+    canvas = plotCollection.Plotter.canvas
+
+    fin = ROOT.TFile(fname)
+
+    def _plot(hname):
+        h = fin.Get(hname)
+
+        # Take the value from single Gaussian for the Kshort window
+        #  f = ROOT.TF1("f", "[0]*exp(-0.5*((x-[1])/[2])**2)", 0.45, 0.55)
+        #  f.SetParameter(1, 0.492)
+        #  f.SetParLimits(2, 0.001, 0.008)
+        #  f.SetParameter(2, 0.006)
+
+        # Double Gaussian looks fine
+        f = ROOT.TF1("f", "[0]*exp(-0.5*((x-[1])/[2])**2)+[3]*exp(-0.5*((x-[1])/[4])**2)", 0.45, 0.55)
+        f.SetParameter(1, 0.492)
+        f.SetParLimits(2, 0.001, 0.008)
+        f.SetParameter(2, 0.006)
+        f.SetParameter(4, 0.01)
+        f.SetParLimits(4, 0.008, 0.05)
+
+        # Breit-Wigner also looks fine.
+        #  f = ROOT.TF1("f", "[0]/((x-[1])**2+([2]**2)/4)/(2*3.1415926)", 0.45, 0.55)
+        #  f.SetParameter(1, 0.492)
+        #  f.SetParLimits(2, 0.001, 0.008)
+        #  f.SetParameter(2, 0.006)
+
+        h.Fit("f", "", "", 0.48, 0.515)
+        h.SetXTitle("m_{#pi^{+}#pi^{-}}")
+        h.Draw()
+
+        highlight_SR = ROOT.TBox(0.417, h.GetYaxis().GetXmin(), 0.577, h.GetYaxis().GetXmax())
+        highlight_SR.SetFillColorAlpha(ROOT.kRed, 0.3)
+        highlight_SR.Draw()
+
+        plotCollection.Plotter.latexDataMarks()
+        canvas.Update()
+        canvas.Print("{0}.pdf".format(hname))
+
+    for hname in ["h_kshortMass_presel"]:
+        _plot(hname)
+
+if __name__ == '__main__':
+    if not os.path.exists("h_plotPreSelector.root"):
+        create_histo()
+    plot_histo()
