0e2167a6041686f5e83eebe9da4298a2e4de22c9
Po-Hsun Chen <pchen@cern.ch>
2019-11-22 11:25:17 +0100

    Check phi efficiency

---
+++ SingleBuToKstarMuMuFitter/script/plotPhiEffi.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import os
+import re
+import ROOT
+
+from SingleBuToKstarMuMuFitter.anaSetup import q2bins, bMassRegions, cuts, cut_kshortWindow, modulePath
+from SingleBuToKstarMuMuFitter.dataCollection import sigMCReader
+from SingleBuToKstarMuMuFitter.plotCollection import Plotter
+
+foutName = "plotPhiEffi.root"
+targetBins = ['belowJpsi', 'betweenPeaks', 'abovePsi2s', 'summary']
+
+def create_histo(cfg=None):
+    forceRebuild = False
+
+    setupEfficiencyBuildProcedure = {
+        'ifiles': sigMCReader.cfg['ifile'],
+        'baseString': "1>0",
+        'cutString': "Bmass > 0.5 && ({0})".format(cuts[-1]),
+    }
+
+    if not os.path.exists(foutName) or forceRebuild:
+        hists = []
+
+        treein = ROOT.TChain("tree")
+        for f in setupEfficiencyBuildProcedure['ifiles']:
+            treein.Add(f)
+
+        df = ROOT.RDataFrame(treein)\
+                .Filter(setupEfficiencyBuildProcedure['baseString'])\
+                .Filter(bMassRegions['Fit']['cutString'])
+
+        for binKey in targetBins:
+            hists.append(df.Filter(re.sub("Mumumass", "sqrt(genQ2)", q2bins[binKey]['cutString']))\
+                .Histo1D(("h_genPhi_{0}".format(binKey), "", 73, -3.15, 3.15), "genPhi"))
+
+            hists.append(df.Filter(q2bins[binKey]['cutString'])\
+                .Filter(setupEfficiencyBuildProcedure['cutString'])\
+                .Histo1D(("h_Phi_{0}".format(binKey), "", 73, -3.15, 3.15), "Phi"))
+
+        fout = ROOT.TFile(foutName, "UPDATE")
+        for h in hists:
+            h.Write()
+        fout.Close()
+    pass
+
+def plot_histo():
+    canvas = Plotter.canvas
+    canvas.cd()
+
+    if os.path.exists(foutName):
+        fin = ROOT.TFile(foutName)
+
+        for binKey in targetBins:
+            h_rec = ROOT.TEfficiency(fin.Get("h_Phi_{0}".format(binKey)), fin.Get("h_genPhi_{0}".format(binKey)))
+            gr = h_rec.CreateGraph()
+            gr.SetMinimum(0)
+            gr.SetMaximum(1)
+            gr.GetXaxis().SetTitle("#phi")
+            gr.GetYaxis().SetTitle("Efficiency")
+            gr.Draw("AP")
+            canvas.Update()
+            canvas.Print("h_rec_Phi_{0}.pdf".format(q2bins[binKey]['label']))
+        fin.Close()
+    pass
+
+if __name__ == "__main__":
+    create_histo()
+    plot_histo()
+    pass
