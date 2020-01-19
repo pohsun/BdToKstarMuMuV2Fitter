d61190f5b6f2d0fe2fa9f3073df83024686b83c6
Po-Hsun Chen <pchen@cern.ch>
2020-01-19 16:26:47 +0100

    Data-MC discrepancy.

--- SingleBuToKstarMuMuFitter/systCollection.py
+++ SingleBuToKstarMuMuFitter/systCollection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# vim: set sts=4 sw=4 fdm=indent fdn=3 et:
+# vim: set sts=4 sw=4 fdm=indent fdl=0 fdn=2 et:
 
-from __future__ import print_function
+from __future__ import print_function, division
 
 import os
 import sys
@@ -9,7 +9,8 @@ import math
 import types
 import shelve
 import functools
-from copy import deepcopy
+import itertools
+from copy import copy, deepcopy
 
 import ROOT
 import SingleBuToKstarMuMuFitter.cpp
@@ -17,11 +18,13 @@ import SingleBuToKstarMuMuFitter.cpp
 from v2Fitter.Fitter.FitterCore import FitterCore
 from v2Fitter.Fitter.AbsToyStudier import AbsToyStudier
 from v2Fitter.Fitter.DataReader import DataReader
-from SingleBuToKstarMuMuFitter.anaSetup import q2bins, cut_kshortWindow
+from SingleBuToKstarMuMuFitter.anaSetup import modulePath, q2bins, bMassRegions, cuts_antiResVeto
 from SingleBuToKstarMuMuFitter.StdFitter import StdFitter, unboundFlToFl, unboundAfbToAfb
+from SingleBuToKstarMuMuFitter.EfficiencyFitter import EfficiencyFitter
 from SingleBuToKstarMuMuFitter.FitDBPlayer import FitDBPlayer
 from SingleBuToKstarMuMuFitter.plotCollection import Plotter, plotter
 
+import SingleBuToKstarMuMuFitter.varCollection as varCollection
 import SingleBuToKstarMuMuFitter.dataCollection as dataCollection
 import SingleBuToKstarMuMuFitter.pdfCollection as pdfCollection
 import SingleBuToKstarMuMuFitter.fitCollection as fitCollection
@@ -29,6 +32,205 @@ import SingleBuToKstarMuMuFitter.fitCollection as fitCollection
 from SingleBuToKstarMuMuFitter.StdProcess import p
 
 from argparse import ArgumentParser
+
+# Data-MC discrepancy in terms of efficiency map
+# # Determinded by reweight the efficiency map derived from MC with a data-MC ratio from Jpsi CR.
+# # Redo the whole fitting procedure using the modified efficiency map.
+def create_histo_data2expt(kwargs=None):
+    kwargs = {} if kwargs is None else kwargs
+    wgtConfig = ["1*(fabs(Bmass-5.28)<0.06) - 0.5*(fabs(Bmass-5.11)<0.06) - 0.5*(fabs(Bmass-5.46)<0.06)",]
+
+    # Data
+    tree_data = ROOT.TChain("tree")
+    tree_data.Add("/eos/cms/store/user/pchen/BToKstarMuMu/dat/sel/ANv21/DATA/*.root")
+    df_data = ROOT.RDataFrame(tree_data).Define('weight', *wgtConfig).Filter(cuts_antiResVeto).Filter(q2bins['jpsi']['cutString'])
+
+    ptr_h_CosThetaL_data = df_data.Histo1D(("h_CosThetaL", "", 20, -1, 1), "CosThetaL", "weight")
+    ptr_h_CosThetaK_data = df_data.Histo1D(("h_CosThetaK", "", 20, -1, 1), "CosThetaK", "weight")
+    ptr_h2_sigA_data = df_data.Histo2D(
+        ("h2_sigA_data", "",
+            len(dataCollection.accXEffThetaLBins) - 1, dataCollection.accXEffThetaLBins, 
+            len(dataCollection.accXEffThetaKBins) - 1, dataCollection.accXEffThetaKBins),
+        "CosThetaL",
+        "CosThetaK",
+        "weight"
+    )
+
+    # (Unfiltered) MC
+    tree_expt = ROOT.TChain("tree")
+    tree_expt.Add("/eos/cms/store/user/pchen/BToKstarMuMu/dat/sel/v3p5/unfilteredJPSI_genonly/*.root")
+    df_expt = ROOT.RDataFrame(tree_expt).Define('genWeight', "1").Filter("genQ2 > 8.68 && genQ2 < 10.09")
+
+    ptr_h_CosThetaL_mc = df_expt.Histo1D(("h_genCosThetaL", "", 20, -1, 1), "genCosThetaL", "genWeight")
+    ptr_h_CosThetaK_mc = df_expt.Histo1D(("h_genCosThetaK", "", 20, -1, 1), "genCosThetaK", "genWeight")
+    ptr_h2_sigA_mc = df_expt.Histo2D(
+        ("h2_sigA_mc", "",
+            len(dataCollection.accXEffThetaLBins) - 1, dataCollection.accXEffThetaLBins, 
+            len(dataCollection.accXEffThetaKBins) - 1, dataCollection.accXEffThetaKBins),
+        "genCosThetaL",
+        "genCosThetaK",
+        "genWeight"
+    )
+
+    # Efficiency
+    effiFile = ROOT.TFile(modulePath + "/input/wspace_bin2.root")
+    effiWspace = effiFile.Get("wspace.bin2")
+    effi_sigA = effiWspace.function("effi_sigA")
+    effi_cosl = effiWspace.function("effi_cosl")
+    effi_cosK = effiWspace.function("effi_cosK")
+    FitDBPlayer.initFromDB(
+        modulePath + "/testProcess/fitResults_bin2.db",
+        effi_sigA.getParameters(ROOT.RooArgSet(varCollection.CosThetaK, varCollection.CosThetaL)))
+    FitDBPlayer.initFromDB(
+        modulePath + "/testProcess/fitResults_bin2.db",
+        effi_cosl.getParameters(ROOT.RooArgSet(varCollection.CosThetaL)))
+    FitDBPlayer.initFromDB(
+        modulePath + "/testProcess/fitResults_bin2.db",
+        effi_cosK.getParameters(ROOT.RooArgSet(varCollection.CosThetaK)))
+    h2_effi_sigA = effi_sigA.createHistogram("h2_effi_sigA",
+        varCollection.CosThetaL,
+        ROOT.RooFit.Binning(dataCollection.rAccXEffThetaLBins),
+        ROOT.RooFit.YVar(varCollection.CosThetaK, ROOT.RooFit.Binning(dataCollection.rAccXEffThetaKBins)))
+    h_effi_cosl = effi_cosl.createHistogram("h_effi_cosl",
+        varCollection.CosThetaL,
+        ROOT.RooFit.Binning(20))
+    h_effi_cosK = effi_cosK.createHistogram("h_effi_cosK",
+        varCollection.CosThetaK,
+        ROOT.RooFit.Binning(20))
+
+    # Expected
+    h2_sigA_expc = h2_effi_sigA.Clone("h2_sigA_expc")
+    h2_sigA_expc.Reset("ICESM")
+    for xBin, yBin in itertools.product(range(1, len(dataCollection.accXEffThetaLBins)), range(1, len(dataCollection.accXEffThetaKBins))):
+        iBin = h2_sigA_expc.GetBin(xBin, yBin)
+        h2_sigA_expc.SetBinContent(iBin, ptr_h2_sigA_mc.GetValue().GetBinContent(iBin) * h2_effi_sigA.GetBinContent(iBin))
+    h2_sigA_expc.Scale(ptr_h_CosThetaL_data.Integral()/h2_sigA_expc.Integral())
+    h_cosl_expc = h_effi_cosl.Clone("h_cosl_expc")
+    h_cosK_expc = h_effi_cosK.Clone("h_cosK_expc")
+    h_cosl_expc.Reset("ICESM")
+    h_cosK_expc.Reset("ICESM")
+    for iBin in range(1, 20+1):
+        h_cosl_expc.SetBinContent(iBin, ptr_h_CosThetaL_mc.GetValue().GetBinContent(iBin) * h_effi_cosl.GetBinContent(iBin))
+        h_cosK_expc.SetBinContent(iBin, ptr_h_CosThetaK_mc.GetValue().GetBinContent(iBin) * h_effi_cosK.GetBinContent(iBin))
+    h_cosl_expc.Scale(ptr_h_CosThetaL_data.Integral()/h_cosl_expc.Integral())
+    h_cosK_expc.Scale(ptr_h_CosThetaK_data.Integral()/h_cosK_expc.Integral())
+
+    # Ratio
+    h2_sigA_ratio = h2_effi_sigA.Clone("h2_sigA_ratio")
+    h2_sigA_ratio.Reset("ICESM")
+    for xBin, yBin in itertools.product(range(1, len(dataCollection.accXEffThetaLBins)), range(1, len(dataCollection.accXEffThetaKBins))):
+        iBin = h2_sigA_ratio.GetBin(xBin, yBin)
+        h2_sigA_ratio.SetBinContent(iBin, ptr_h2_sigA_data.GetValue().GetBinContent(iBin) / h2_sigA_expc.GetBinContent(iBin))
+    h_cosl_ratio = h_effi_cosl.Clone("h_cosl_ratio")
+    h_cosK_ratio = h_effi_cosK.Clone("h_cosK_ratio")
+    h_cosl_ratio.Reset("ICESM")
+    h_cosK_ratio.Reset("ICESM")
+    for iBin in range(1, 20+1):
+        h_cosl_ratio.SetBinContent(iBin, ptr_h_CosThetaL_data.GetValue().GetBinContent(iBin) / h_cosl_expc.GetBinContent(iBin))
+        h_cosK_ratio.SetBinContent(iBin, ptr_h_CosThetaK_data.GetValue().GetBinContent(iBin) / h_cosK_expc.GetBinContent(iBin))
+
+    fout = ROOT.TFile(modulePath + "/systCollection_dataMCDisc.root", "RECREATE")
+    hists_to_write = [
+        ptr_h_CosThetaL_data.GetValue(),
+        ptr_h_CosThetaK_data.GetValue(),
+        ptr_h_CosThetaL_mc.GetValue(),
+        ptr_h_CosThetaK_mc.GetValue(),
+        ptr_h2_sigA_data.GetValue(),
+        ptr_h2_sigA_mc.GetValue(),
+        h_effi_cosl,
+        h_cosl_expc,
+        h_cosl_ratio,
+        h_effi_cosK,
+        h_cosK_expc,
+        h_cosK_ratio,
+        h2_effi_sigA,
+        h2_sigA_expc,
+        h2_sigA_ratio,
+    ]
+    for h in hists_to_write:
+        h.SetTitle("")
+        h.Write()
+    fout.Write()
+    fout.Close()
+
+def func_dataMCDisc(args):
+    """Redo final fit with weighted efficiency map, where weight from JPsi CR."""
+    if not os.path.exists(modulePath + "/systCollection_dataMCDisc.root"):
+        create_histo_data2expt()
+
+    weightFile= ROOT.TFile("systCollection_dataMCDisc.root")
+    h_cosl_ratio = weightFile.Get("h_cosl_ratio")
+    h_cosK_ratio = weightFile.Get("h_cosK_ratio")
+    h2_sigA_expc = weightFile.Get("h2_sigA_expc")
+    h2_sigA_data = weightFile.Get("h2_sigA_data")
+    h2_weight = h2_sigA_expc.Clone("h2_sigA_expc")
+    h2_weight.Reset("ICESM")
+    for xBin, yBin in itertools.product(range(1, h2_weight.GetNbinsX()+1), range(1, h2_weight.GetNbinsY()+1)):
+        iBin = h2_weight.GetBin(xBin, yBin)
+        h2_weight.SetBinContent(iBin, h2_sigA_data.GetBinContent(iBin)/h2_sigA_expc.GetBinContent(iBin))
+        print("Weight in bin("+str(xBin)+","+str(yBin)+") = "+str(h2_weight.GetBinContent(iBin)))
+
+    accXrecEffHistFile = ROOT.TFile(modulePath + "/data/accXrecEffHists_Run2012.root")
+    h2_accXrec = accXrecEffHistFile.Get("h2_accXrec_{0}".format(args.binKey))
+    h2_accXrec_weight = h2_accXrec.Clone("h2_accXrec_{0}_weight".format(args.binKey))
+    h2_accXrec_weight.Reset("ICESM")
+    for xBin, yBin in itertools.product(range(1, h2_weight.GetNbinsX()+1), range(1, h2_weight.GetNbinsY()+1)):
+        iBin = h2_weight.GetBin(xBin, yBin)
+        h2_accXrec_weight.SetBinContent(iBin, h2_accXrec.GetBinContent(iBin) * h2_weight.GetBinContent(iBin))
+        h2_accXrec_weight.SetBinError(iBin, h2_accXrec.GetBinError(iBin))
+    h2_accXrec.Print()
+    h2_accXrec_weight.Print()
+    p.sourcemanager.update("h2_accXrec_weight", h2_accXrec_weight)
+    
+    h_accXrec_ProjectionX = accXrecEffHistFile.Get("h_accXrec_{0}_ProjectionX".format(args.binKey))
+    h_accXrec_ProjectionY = accXrecEffHistFile.Get("h_accXrec_{0}_ProjectionY".format(args.binKey))
+    h_accXrec_ProjectionX_weight = h_accXrec_ProjectionX.Clone("h_accXrec_{0}_ProjectionX_weight".format(args.binKey))
+    h_accXrec_ProjectionY_weight = h_accXrec_ProjectionY.Clone("h_accXrec_{0}_ProjectionY_weight".format(args.binKey))
+    h_accXrec_ProjectionX_weight.Clone("ICESM")
+    h_accXrec_ProjectionY_weight.Clone("ICESM")
+    for iBin in range(1, 20+1):
+        h_accXrec_ProjectionX_weight.SetBinContent(iBin, h_accXrec_ProjectionX.GetBinContent(iBin) * h_cosl_ratio.GetBinContent(iBin))
+        h_accXrec_ProjectionY_weight.SetBinContent(iBin, h_accXrec_ProjectionY.GetBinContent(iBin) * h_cosK_ratio.GetBinContent(iBin))
+        h_accXrec_ProjectionX_weight.SetBinError(iBin, h_accXrec_ProjectionX.GetBinError(iBin))
+        h_accXrec_ProjectionY_weight.SetBinError(iBin, h_accXrec_ProjectionY.GetBinError(iBin))
+    p.sourcemanager.update("h_accXrec_ProjectionX_weight", h_accXrec_ProjectionX_weight)
+    p.sourcemanager.update("h_accXrec_ProjectionY_weight", h_accXrec_ProjectionY_weight)
+
+    setupWeightEffiFitter = deepcopy(fitCollection.setupEffiFitter)
+    setupWeightEffiFitter.update({
+        'hdata': "h2_accXrec_weight",
+        'dataX': "h_accXrec_ProjectionX_weight",
+        'dataY': "h_accXrec_ProjectionY_weight",
+        'noDraw': True,
+        'saveToDB': False
+    })
+    weightEffiFitter = EfficiencyFitter(setupWeightEffiFitter)
+
+    setupFinalAltEffiFitter = deepcopy(fitCollection.setupFinalFitter)
+    setupFinalAltEffiFitter.update({
+        'argAliasInDB': {'afb': 'afb_dataMCDisc', 'fl': 'fl_dataMCDisc', 'fs': 'fs_dataMCDisc', 'as': 'as_dataMCDisc'},
+        'saveToDB': False,
+    })
+    finalAltEffiFitter = StdFitter(setupFinalAltEffiFitter)
+
+    p.setSequence([
+        pdfCollection.stdWspaceReader,
+        dataCollection.effiHistReader,
+        dataCollection.dataReader,
+        weightEffiFitter,
+        finalAltEffiFitter,
+    ])
+
+    try:
+        p.beginSeq()
+        p.runSeq()
+
+        updateToDB_altShape(args, "dataMCDisc")
+    finally:
+        p.endSeq()
+    accXrecEffHistFile.Close()
+    weightFile.Close()
+    pass
 
 # Limited MC size
 # # Determinded by varying efficiency map with FitDBPlayer.fluctuateFromDB.
@@ -394,8 +596,7 @@ def func_altFitRange(args):
     dataReader = DataReader(dataReaderCfg)
     dataReader.customize = types.MethodType(
         functools.partial(dataCollection.customizeOne,
-                          targetBMassRegion=['^altFit$'],
-                          extraCuts=cut_kshortWindow),
+                          targetBMassRegion=['^altFit$']),
         dataReader
     )
     fitterCfg = deepcopy(fitCollection.setupFinalFitter)
@@ -429,8 +630,7 @@ def func_vetoJpsiX(args):
     dataReader = DataReader(dataReaderCfg)
     dataReader.customize = types.MethodType(
         functools.partial(dataCollection.customizeOne,
-                          targetBMassRegion=['altFit_vetoJpsiX'],
-                          extraCuts=cut_kshortWindow),
+                          targetBMassRegion=['altFit_vetoJpsiX']),
         dataReader
     )
     fitterCfg = deepcopy(fitCollection.setupFinalFitter)
@@ -519,12 +719,15 @@ if __name__ == '__main__':
 
     subparsers = parser.add_subparsers(help="Functions", dest='Function_name')
 
+    subparser_dataMCDisc = subparsers.add_parser('dataMCDisc')
+    subparser_dataMCDisc.set_defaults(func=func_dataMCDisc)
+
     subparser_randEffi = subparsers.add_parser('randEffi')
     subparser_randEffi.set_defaults(func=func_randEffi)
 
     subparser_altEffi = subparsers.add_parser('altEffi')
     subparser_altEffi.set_defaults(func=func_altEffi)
-    
+
     subparser_altEffi2 = subparsers.add_parser('altEffi2')
     subparser_altEffi2.set_defaults(func=func_altEffi2)
 
